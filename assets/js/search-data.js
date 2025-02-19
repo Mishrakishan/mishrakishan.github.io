0634dc3b522248e266eebb97227323146be195dc
Mishrakishan <68537575+Mishrakishan@users.noreply.github.com>
2025-02-19 10:40:36 +0000

    Deploying to gh-pages from @ Mishrakishan/mishrakishan.github.io@fd69ed88bc2459d957fad246092ff3ded7334822 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-quantum-mechanics-101-demystifying-its-postulates",
+        },{id: "post-trial-post",
+      
+        title: "Trial post",
+      
+      description: "An introductory look at the core principles of quantum mechanics.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/trial/";
+        
+      },
+    },{id: "post-quantum-mechanics-101-demystifying-its-postulates",
       
         title: "Quantum Mechanics 101: Demystifying Its Postulates",
       
